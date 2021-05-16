6e214e2780157eff088bccc780d02474caabda69
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-05-16 07:03:43 +0600

    test: for y2015 d01 p2

--- test/2015/day_1_test.exs
+++ test/2015/day_1_test.exs
@@ -29,4 +29,19 @@ defmodule AdventOfCode.Y2015.Day1Test do
       assert Solution.floor(" ") == {:error, :invalid}
     end
   end
+
+  describe "testing Y2015 Day1 Part2" do
+    test "single closing parenthesis causes santa to go directly in the basement" do
+      assert Solution.to_basement(")") == {:ok, 1}
+    end
+
+    test "when the number of ')' is higher it causes santa to go into basement" do
+      assert Solution.to_basement("()())") == {:ok, 5}
+    end
+
+    test "empty input should return an error" do
+      assert Solution.floor("") == {:error, :invalid}
+      assert Solution.floor(" ") == {:error, :invalid}
+    end
+  end
 end
