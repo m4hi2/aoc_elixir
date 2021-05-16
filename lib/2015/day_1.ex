5aae6625c50309eb63568d33f79ace76b0af6950
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-05-16 08:10:35 +0600

    feat: gibrish implementation for day1 part2

--- lib/2015/day_1.ex
+++ lib/2015/day_1.ex
@@ -21,4 +21,35 @@ defmodule AdventOfCode.Y2015.Day1 do
 
     {:ok, result}
   end
+
+  @spec to_basement(binary) :: {:error, :invalid} | {:ok, any}
+  def to_basement(""), do: {:error, :invalid}
+  def to_basement(" "), do: {:error, :invalid}
+
+  def to_basement(instructions) when is_binary(instructions) do
+    pos = 0
+
+    result =
+      instructions
+      |> String.graphemes()
+      |> Enum.reduce_while(0, fn x, acc ->
+        cond do
+          pos == -1 ->
+            {:halt, acc}
+
+          pos > -1 ->
+            case x do
+              "(" ->
+                pos = pos + 1
+                {:cont, acc + 1}
+
+              ")" ->
+                pos = pos - 1
+                {:cont, acc + 1}
+            end
+        end
+      end)
+
+    {:ok, result}
+  end
 end
