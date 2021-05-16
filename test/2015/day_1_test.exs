bb26910fb61ccf11af2145fd0e5b2beff7eb34c2
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-05-16 06:02:50 +0600

    test: necessary test cases for y15d01

---
+++ test/2015/day_1_test.exs
@@ -0,0 +1,32 @@
+defmodule AdventOfCode.Y2015.Day1Test do
+  @moduledoc false
+  use ExUnit.Case
+  @moduletag :y15d01
+
+  alias AdventOfCode.Y2015.Day1, as: Solution
+
+  describe "testing Y2015 Day1 Part 1" do
+    test "floor level is returned '0' for even number of both parenthesis" do
+      assert Solution.floor("(())") == {:ok, 0}
+      assert Solution.floor("()()") == {:ok, 0}
+    end
+
+    test "floor level will be greater than '0' if the number of '(' is higher" do
+      assert Solution.floor("(((") == {:ok, 3}
+      assert Solution.floor("(()(()(") == {:ok, 3}
+      assert Solution.floor("))(((((") == {:ok, 3}
+    end
+
+    test "floor level will be less than '0' if the number is ')' is higher" do
+      assert Solution.floor("())") == {:ok, -1}
+      assert Solution.floor("))(") == {:ok, -1}
+      assert Solution.floor(")))") == {:ok, -3}
+      assert Solution.floor(")())())") == {:ok, -3}
+    end
+
+    test "empty input should return an error" do
+      assert Solution.floor("") == {:error, :invalid}
+      assert Solution.floor(" ") == {:error, :invalid}
+    end
+  end
+end
