c1a1ad0ce13c38e33c72d8b39b9c4ed350673bfb
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-12-03 05:02:38 +0600

    reformatted code

--- lib/2021/day_2.ex
+++ lib/2021/day_2.ex
@@ -11,23 +11,22 @@ defmodule AdventOfCode.Y2021.Day2 do
     |> calc_pos()
   end
 
-  def process_input(input) do
-    input
-    |> String.split("\n")
-    |> Enum.map(&String.split(&1, " "))
-    |> Enum.map(fn [command, value] -> [command, String.to_integer(value)] end)
-  end
-
   def calc_pos(commands) when is_list(commands), do: calc_pos(commands, 0, 0)
   def calc_pos([], depth, h_pos), do: depth * h_pos
   def calc_pos([["forward", n] | tail], depth, h_pos), do: calc_pos(tail, depth, h_pos + n)
   def calc_pos([["up", n] | tail], depth, h_pos), do: calc_pos(tail, depth - n, h_pos)
   def calc_pos([["down", n] | tail], depth, h_pos), do: calc_pos(tail, depth + n, h_pos)
 
-
-    defp load_file do
+  defp load_file do
     file_path = Path.join(:code.priv_dir(:advent_of_code), "2021/day_2.txt")
     {:ok, file} = File.read(file_path)
     file
+  end
+
+  defp process_input(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, " "))
+    |> Enum.map(fn [command, value] -> [command, String.to_integer(value)] end)
   end
 end
