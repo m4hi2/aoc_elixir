a9b4317a0b4e92eab1f0258c4b2a059154b5d7df
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-12-03 04:10:09 +0600

    test: sample input

--- test/2021/day_2_test.exs
+++ test/2021/day_2_test.exs
@@ -3,6 +3,19 @@ defmodule AdventOfCode.Y2021.Day2Test do
   use ExUnit.Case
   @moduletag :y21d02
 
+  @sampleinput "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"
+
   alias AdventOfCode.Y2021.Day2, as: Solution
+
+  describe "problem example should get solved to " do
+    test "150" do
+      assert Solution.solve_1(@sampleinput) == 150
+    end
+  end
 
 end
