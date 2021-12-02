8e138701b71eac52d5b21ab1a7af0f7109c61ae1
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-12-02 06:03:44 +0600

    feat: soln to part 1

---
+++ lib/2021/day_1.ex
@@ -0,0 +1,32 @@
+defmodule AdventOfCode.Y2021.Day1 do
+  def run_part1 do
+    file_path = Path.join(:code.priv_dir(:advent_of_code), "2021/day_1.txt")
+    {:ok, file} = File.read(file_path)
+
+    file
+    |> solve_part1()
+  end
+
+  def solve_part1(depths) do
+    depths
+    |> String.split()
+    |> Enum.map(&String.to_integer(&1))
+    |> find_increment(0)
+  end
+
+  defp find_increment(list, 0) do
+    [h | t] = list
+    find_increment(h, t, 0)
+  end
+
+  defp find_increment(_, [], acc), do: {:ok, acc}
+
+  defp find_increment(h, t, acc) do
+    [next | rest] = t
+
+    case next > h do
+      true -> find_increment(next, rest, acc + 1)
+      _ -> find_increment(next, rest, acc)
+    end
+  end
+end
