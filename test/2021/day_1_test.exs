e55d9767fea4b84d4407f9baf0f45d854fe7e572
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-12-02 06:03:28 +0600

    test: based on example problem

---
+++ test/2021/day_1_test.exs
@@ -0,0 +1,24 @@
+defmodule AdventOfCode.Y2021.Day1Test do
+  @moduledoc false
+  use ExUnit.Case
+  @moduletag :y21d01
+
+  alias AdventOfCode.Y2021.Day1, as: Solution
+
+  @sample_input "199
+200
+208
+210
+200
+207
+240
+269
+260
+263"
+
+  describe "exapme provided in the aoc listing should" do
+    test "increased by 7" do
+      assert Solution.solve_part1(@sample_input) == {:ok, 7}
+    end
+  end
+end
